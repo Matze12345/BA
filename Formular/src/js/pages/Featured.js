dd95901756a45697f0fd414b8942af3b2605ef88
matze12345 <94matze@web.de>
2017-11-22 08:47:18 +0100

    Projekt angelegt

---
+++ Formular/src/js/pages/Featured.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+export default class Featured extends React.Component {
+  render() {
+
+
+
+    console.log("featured");
+    return (
+      <div>
+        <div class="row">
+          <div class="col-lg-12">
+
+          </div>
+        </div>
+
+      </div>
+    );
+  }
+}
