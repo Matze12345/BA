8287ec86c9dce2c097fb19cdfd20fba94cba300e
matze12345 <94matze@web.de>
2017-11-25 15:06:55 +0100

    Update

--- Formular/src/js/pages/Home.js
+++ Formular/src/js/pages/Home.js
@@ -1,13 +1,17 @@
 import React from "react";
-import { Button, FormGroup, FormControl, Checkbox, Radio, ControlLabel, HelpBlock } from 'react-bootstrap';
+import { Button, FormGroup, FormControl, ControlLabel, Col, Form, HelpBlock } from 'react-bootstrap';
 import * as ReactBootstrap from 'react-bootstrap';
 
-function FieldGroup({ id, label, help, ...props }) {
+function FieldGroup({ id, label, inputCol, labelCol, valState, size, ...props }) {
     return (
-        <FormGroup controlId={id}>
-            <ControlLabel>{label}</ControlLabel>
-            <FormControl {...props} />
-            {help && <HelpBlock>{help}</HelpBlock>}
+        <FormGroup controlId={id} validationState={valState}  >
+            <Col componentClass={ControlLabel} sm={labelCol}>
+                {label}
+            </Col>
+            <Col sm={inputCol}>
+                <FormControl {...props}/>
+                <FormControl.Feedback />
+            </Col>
         </FormGroup>
     );
 }
@@ -18,6 +22,40 @@ export default class Home extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            text: "",
+            email: "",
+            pw: "",
+            size: "small",
+            valStateText: null,
+            valStateEmail: null,
+            valStatePw: null,
+            labelCol: 4,
+            inputCol: 5,
+        }
+    }
+
+    buttonclick(){
+        if(this.state.text == "" ){
+            this.setState({valStateText: "error"})
+        } else{
+            this.setState({valStateText: null})
+        }
+        if(this.state.email == "" ){
+            this.setState({valStateEmail: "error"})
+        } else{
+            this.setState({valStateEmail: null})
+        }
+        if(this.state.pw == "" ){
+            this.setState({valStatePw: "error"})
+        } else{
+            this.setState({valStatePw: null})
+        }
+
+    }
+
+    changeStatus(event) {
+        this.setState({ [event.target.name]: event.target.value})
     }
 
 
@@ -25,36 +63,56 @@ export default class Home extends React.Component {
   render() {
 
       const formInstance = (
-          <form>
+          <Form horizontal>
               <FieldGroup
                   id="formControlsText"
+                  name="text"
                   type="text"
                   label="Text"
                   placeholder="Enter text"
+                  labelCol={this.state.labelCol}
+                  inputCol={this.state.inputCol}
+                  valState = {this.state.valStateText}
+                  bsSize={this.state.size}
+                  onChange={this.changeStatus.bind(this)}
               />
               <FieldGroup
                   id="formControlsEmail"
+                  name="email"
                   type="email"
                   label="Email address"
                   placeholder="Enter email"
+                  labelCol={this.state.labelCol}
+                  inputCol={this.state.inputCol}
+                  valState = {this.state.valStateEmail}
+                  bsSize={this.state.size}
+                  onChange={this.changeStatus.bind(this)}
               />
               <FieldGroup
                   id="formControlsPassword"
+                  name="pw"
                   label="Password"
                   type="password"
+                  labelCol={this.state.labelCol}
+                  inputCol={this.state.inputCol}
+                  valState = {this.state.valStatePw}
+                  bsSize={this.state.size}
+                  onChange={this.changeStatus.bind(this)}
               />
-              <Button type="submit">
-                  Submit
-              </Button>
-          </form>
+              <FormGroup>
+                  <Col smOffset={this.state.labelCol} sm={2}>
+                      <Button onClick={this.buttonclick.bind(this)} bsSize={this.state.size}>
+                          Weiter
+                      </Button>
+                  </Col>
+              </FormGroup>
+          </Form>
       );
 
     return (
       <div>
         <div class="row">
-          <div class="col-lg-12">
-              {formInstance}
-          </div>
+            <div >  {formInstance} </div>
         </div>
       </div>
     );
