b34929d06cbb009e7aeabcda9f71504d57fa95e4
matze12345 <94matze@web.de>
2017-12-30 12:04:52 +0100

    vorgänger berechnung in react

--- Formular_ai/src/js/pages/Home.js
+++ Formular_ai/src/js/pages/Home.js
@@ -1,16 +1,20 @@
 import React from "react";
-import * as ReactBootstrap from 'react-bootstrap';
-import ReactStars from 'react-stars'
 import {connect} from "react-redux"
 import { fetchInit } from "../actions/initAction"
 
 import { Form } from 'semantic-ui-react'
+
+var test;
+test = [];
+
 
 @connect((store) => {
     return {
         NewInit: store.initRed.initR,
     };
 })
+
+
 
 export default class Home extends React.Component {
 
@@ -23,19 +27,13 @@ export default class Home extends React.Component {
             hnr: "",
             plz: "",
             ort: "",
-            clickv: "",
-            clickn: "",
-            clicks: "",
-            clickh: "",
-            clickp: "",
-            clicko: "",
-            points: 6,
+            vorg: 0,
             click: [],
         }
     }
 
     componentWillMount() {
-         this.props.dispatch(fetchInit(this.state.clickv, this.state.clickn, this.state.clicks, this.state.clickh, this.state.clickp, this.state.clicko))
+         this.props.dispatch(fetchInit(this.state.click))
     }
 
      handleChange = (e, { name, value }) => {
@@ -45,40 +43,39 @@ export default class Home extends React.Component {
      handleSubmit = () => {
          this.props.dispatch(fetchInit(this.state.click))
          this.setState({click: []})
-         //this.setState({points: 6})
-         //this.props.dispatch(fetchInit(this.state.clickv, this.state.clickn, this.state.clicks, this.state.clickh, this.state.clickp, this.state.clicko))
      }
 
      handleClick = (e) => {
-         /*var { points } = this.state
-         this.setState({ [e.target.id]: points })
-         points--
-         this.setState({ points: points })*/
-
-         var { click } = this.state
-         click.push({id: e.target.id, skip: "", time: ""})
+         var { click, vorg } = this.state
+         var skip = Math.abs( vorg - test[e.target.id - 1].index - 1 )
+         click.push({id: e.target.id, skip: skip, time: ""})
+         console.log(test[e.target.id - 1].index)
+         console.log("skip:" +skip)
+         var vorganger = test[e.target.id - 1].index
+         console.log(vorganger)
+         this.setState({vorg: vorganger})
      }
 
   render() {
     const array = this.props.NewInit
     const {} = this.state
-    var test = []
 
-    test.push(<Form.Group widths='equal'><Form.Input id="1" label='Vorname' placeholder='Vorname' name='vname'  onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>)
-    test.push(<Form.Group widths='equal'><Form.Input id="2" label='Nachname' placeholder='Nachname' name='nname'  onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>)
-    test.push(<Form.Group widths='equal'><Form.Input id="3" label='Straße' placeholder='Straße' name='str'  onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>)
-    test.push(<Form.Group widths='equal'><Form.Input id="4" label='Hausnummer' placeholder='Hausnummer' name='hnr' width={4}  onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>)
-    test.push(<Form.Group widths='equal'><Form.Input id="5" label='Postleitzahl' placeholder='Plz' name='plz' width={3} onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>)
-    test.push(<Form.Group widths='equal'><Form.Input id="6" label='Ort' placeholder='Ort' name='ort' onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>)
+    test.push({id: 1, index: "", html: <Form.Input id="1" label='Vorname'  placeholder='Vorname' name='vname'  onClick={this.handleClick} onChange={this.handleChange}/> })
+    test.push({id: 2, index: "", html: <Form.Group widths='equal'><Form.Input id="2" label='Nachname' placeholder='Nachname' name='nname'  onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>})
+    test.push({id: 3, index: "", html: <Form.Group widths='equal'><Form.Input id="3" label='Straße' placeholder='Straße' name='str'  onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>})
+    test.push({id: 4, index: "", html: <Form.Group widths='equal'><Form.Input id="4" label='Hausnummer' placeholder='Hausnummer' name='hnr' width={4}  onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>})
+    test.push({id: 5, index: "", html: <Form.Group widths='equal'><Form.Input id="5" label='Postleitzahl' placeholder='Postleitzahl' name='plz' width={3} onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>})
+    test.push({id: 6, index: "", html: <Form.Group widths='equal'><Form.Input id="6" label='Ort' placeholder='Ort' name='ort' onClick={this.handleClick} onChange={this.handleChange}/></Form.Group>})
 
     return (
       <div>
         <div class="row">
              <Form loading={false}>
                 {
-                    array.form.map(function (data) {
+                    array.form.map(function (data, index) {
+                        test[data].index = index
                     return (
-                          test[data]
+                          test[data].html
                         )
                     })
                 }
