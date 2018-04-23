20436b8e67a7a94dfb315d542e0ad1492f9219ac
matze12345 <94matze@web.de>
2018-04-23 17:26:31 +0200

    update

--- Node_dst_er/logic/auswerten.js
+++ Node_dst_er/logic/auswerten.js
@@ -3,13 +3,15 @@ var db = require('../database/dbScore')
 var clustering = require('density-clustering');
 var kmeans = new clustering.KMEANS();
 var optics = new clustering.OPTICS();
+var dbscan = new clustering.DBSCAN();
 
+var skmeans = require("skmeans");
 
 module.exports = {
 
     auswerten: function (callback) {
 
-        db.selectRaw(function (raw, dataTime) {
+        db.selectAutoRaw(function (raw, dataTime) {
             var data = []
             var min = []
             var max = []
@@ -55,7 +57,7 @@ module.exports = {
                 }
                 var moveSpeed = strecke / time
                 //console.log("strecke: " + strecke + " moveSpeed: " + moveSpeed + " inputSpeed: " + inputSpeed + " tab: " + tab + " mouse: " + mouse + " time: " + time)
-                console.log(strecke + " ; " + moveSpeed + " ; " + inputSpeed + " ; " + tab + " ; " + mouse)
+                //  console.log(strecke + " ; " + moveSpeed + " ; " + inputSpeed + " ; " + tab + " ; " + mouse)
 
 
                 if (min.length == 0 && max.length == 0) {
@@ -107,36 +109,77 @@ module.exports = {
                 data.push([strecke, moveSpeed, inputSpeed, tab, mouse, rawTime])
             }
 
+
             var normData = []
             for (var i = 0; i < data.length; i++) {
-                normData.push([((data[i][0] - min[0]) / (max[0] - min[0])) * 10 , ((data[i][1] - min[1]) / (max[1] - min[1])), ((data[i][2] - min[2]) / (max[2] - min[2])), ((data[i][3] - min[3]) / (max[3] - min[3])), ((data[i][4] - min[4]) / (max[4] - min[4]))])
+                normData.push([((data[i][0] - min[0]) / (max[0] - min[0])), ((data[i][1] - min[1]) / (max[1] - min[1])), ((data[i][2] - min[2]) / (max[2] - min[2])), ((data[i][3] - min[3]) / (max[3] - min[3])), ((data[i][4] - min[4]) / (max[4] - min[4]))])
             }
-          //  console.log(normData)
+            //  console.log(normData)
 
             //  console.log(max)
             //  console.log(min)
             // console.log(normData)
 
+
+             var res = skmeans(normData, 4, ["kmpp"], [20000])
+
+            var res = skmeans(normData, 4, [[ 0, 0, 0.2894590972991365, 0, 0.23529411764705882],
+                                           [1,0.48869234924517113,0.17801444477695727,0,0.4117647058823529],
+                                           [0.3490197434864035,0.37453329620947545,0.03905828199092338,0,0.17647058823529413],
+                                            [0.3447312407560549,0.3844999378828552,0.26494284865128725,0.5,0]], [20000])
+            console.log(res)
+            console.log("################################### CLUSTER 1 #################################")
+            console.log(res.idxs)
+            for (var i = 0; i < res.idxs.length; i++) {
+                if (res.idxs[i] == '0') {
+                    console.log(normData[i].toString())
+                }
+            }
+            console.log("################################### CLUSTER 2 #################################")
+            for (var i = 0; i < res.idxs.length; i++) {
+                if (res.idxs[i] == "1") {
+                    console.log(normData[i].toString())
+                }
+            }
+            console.log("################################### CLUSTER 3 #################################")
+            for (var i = 0; i < res.idxs.length; i++) {
+                if (res.idxs[i] == "2") {
+                   console.log(normData[i].toString())
+                }
+            }
+            console.log("################################### CLUSTER 4 #################################")
+            for (var i = 0; i < res.idxs.length; i++) {
+                if (res.idxs[i] == "3") {
+                   console.log(normData[i].toString())
+                }
+            }
+
+
+            var clusters = kmeans.run(normData, 4);
             console.log("####################################################################")
 
+            //var clusters = dbscan.run(normData, 0.2, 2);
 
-            var clusters = kmeans.run(normData, 3);
-            console.log("####################################################################")
-
-
-            //  var clusters = optics.run(data, 1000, 2);
+            //console.log(normData)
+            //  var clusters = optics.run(data, 9000, 3);
             //var plot = optics.getReachabilityPlot();
             //console.log(clusters)
 
             for (var i = 0; i < clusters.length; i++) {
-                 console.log('###############################    CLUSTER ' + (i + 1) + '      #####################################')
+              //   console.log('###############################    CLUSTER ' + (i + 1) + " Länge " + clusters[i].length + '      #####################################')
                 for (var j = 0; j < clusters[i].length; j++) {
-                    console.log(normData[clusters[i][j]])
+                   //    console.log(data[clusters[i][j]].toString())
                     /*   if (clusters[i][j] == (data.length - 1)) {
                      clusternr = i + 1;
                      } */
                 }
             }
+
+            //console.log('###############################    LAST CLUSTER ' + " Länge " + dbscan.noise.length + '      #####################################')
+            //for (var i = 0; i < dbscan.noise.length; i++) {
+            //    console.log(data[dbscan.noise[i]])
+            // }
+            //      console.log(dbscan.noise)
 
 
             callback();
