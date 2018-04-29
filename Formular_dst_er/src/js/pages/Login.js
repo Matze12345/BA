575494ebc8f2779d51974ef316cf92ad3cb90930
matze12345 <94matze@web.de>
2018-04-29 11:51:25 +0200

    update

---
+++ Formular_dst_er/src/js/pages/Login.js
@@ -0,0 +1,97 @@
+import React from "react";
+import {connect} from "react-redux"
+
+import {Router, Route, Link, RouteHandler} from 'react-router';
+
+import {Form, Message, Icon, Modal, Button, Grid} from 'semantic-ui-react'
+
+function validate(pw) {
+    return {
+        pw: pw !== '123456',
+    };
+}
+
+export default class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            pw: "",
+            error: "",
+        }
+    }
+
+    componentWillMount() {
+    }
+
+    handleChange = (e, {name, value}) => {
+        this.setState({[name]: value})
+    }
+
+    handleSubmit = () => {
+        const {pw} = this.state
+        const error = validate(pw)
+        console.log(error)
+        if (error.pw == false) {
+
+            this.setState({
+                pw: "",
+                error: "",
+            }, () => {
+                window.location.assign("/#/" + "home")
+            })
+        } else {
+            this.setState({error: error})
+        }
+    }
+
+    handleClick = (e) => {
+
+    }
+
+
+    render() {
+        const {error, pw} = this.state
+
+        return (
+            <div class="container" style={{marginTop: "60px"}}>
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <div>
+                            <div class="center"> Bitte melden Sie sich an, um die Tests durchzuführen
+                            </div>
+                            <br/>
+                        </div>
+
+                        <Grid verticalAlign='middle' columns={4} centered>
+                            <Grid.Row>
+                                <Grid.Column>
+                                </Grid.Column>
+
+
+                                <Grid.Column>
+                                    <Form onSubmit={this.handleSubmit} size="medium">
+                                        <Form.Group widths='equal'>
+                                            <Form.Input id="2" placeholder='Passwort' name='pw' value={pw}
+                                                        type="password"
+                                                        onChange={this.handleChange} error={error.pw ? "error" : ""}/>
+                                            <Form.Button fluid primary content="Login" size="medium"/>
+                                        </Form.Group>
+
+                                    </Form>
+                                </Grid.Column>
+
+
+                                <Grid.Column>
+                                </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                        
+                        <br/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
